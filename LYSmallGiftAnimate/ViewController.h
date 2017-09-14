fdbd00abfacbca0ca6fa3f728cd0e11817bb727e
李洋 <13363182679@163.com>
2017-09-14 15:13:03 +0800

    Initial Commit

---
+++ LYSmallGiftAnimate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYSmallGiftAnimate
+//
+//  Created by 李洋 on 2017/9/14.
+//  Copyright © 2017年 李洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LYSmallGiftAnimate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LYSmallGiftAnimate
+//
+//  Created by 李洋 on 2017/9/14.
+//  Copyright © 2017年 李洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
